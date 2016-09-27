089874555f2426e6cb6821900e1addc720e9c61e
creativeme <parn.a@creativeme.co.th>
2016-09-27 16:37:08 +0700

    parn test push 2

--- app/src/main/java/th/co/creativeme/testgit/ActivityParn.java
+++ app/src/main/java/th/co/creativeme/testgit/ActivityParn.java
@@ -2,12 +2,17 @@ package th.co.creativeme.testgit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class ActivityParn extends AppCompatActivity {
+
+    private final String TAG =  "ActivityParn";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_parn);
+
+        Log.i(TAG, "Show Log");
     }
 }
--- app/src/main/java/th/co/creativeme/testgit/activity/MainActivity.java
+++ app/src/main/java/th/co/creativeme/testgit/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package th.co.creativeme.testgit;
+package th.co.creativeme.testgit.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import th.co.creativeme.testgit.R;
 
 public class MainActivity extends AppCompatActivity {
 
