e016b78b052fcbceb3dadaf95d549c0661cae5a6
pingsasin <sasin.ping@gmail.com>
2016-09-27 16:58:25 +0700

    ping test add btn in main Activity

--- app/src/main/java/th/co/creativeme/testgit/MainActivity.java
+++ app/src/main/java/th/co/creativeme/testgit/MainActivity.java
@@ -1,13 +1,30 @@
 package th.co.creativeme.testgit;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView hello;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        hello = (TextView) findViewById(R.id.hello);
+        button = (Button) findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                hello.setText("World Hello");
+            }
+        });
+
     }
 }
