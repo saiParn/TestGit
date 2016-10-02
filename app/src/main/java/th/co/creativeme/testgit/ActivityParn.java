2c8c30fbaaf177287720fef7553f9a839a93a41a
Ampornsak Angkatavanich <newparn@msn.com>
2016-10-02 10:16:46 +0700

    new commit to parn3

--- app/src/main/java/th/co/creativeme/testgit/ActivityParn.java
+++ app/src/main/java/th/co/creativeme/testgit/ActivityParn.java
@@ -18,5 +18,6 @@ public class ActivityParn extends AppCompatActivity {
         Log.i(TAG, "Show Log3");
         Log.i(TAG, "Show Log4");
         Log.i(TAG, "Show Log5");
+        Log.i(TAG, "Show Log6");
     }
 }
